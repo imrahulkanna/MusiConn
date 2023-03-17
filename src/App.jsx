f757baeff8509886a8feaec0ca39bb8f875dc3d6
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2023-03-17 16:49:09 +0530

    created the landing page & added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import spotifyLogo from './assets/spotify.svg'
-import './App.css'
-
-function openAuthPage() {
-    const tokenEndPoint = 'https://accounts.spotify.com/api/token';
-    const authEndPoint = 'https://accounts.spotify.com/authorize?';
-    var clientID = '874f362955384d4f82bcd802660e4b0c';
-    var clientSecret = 'acb440a270aa451694ca257703ae07e9';
-
-}
+import { Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={spotifyLogo} className="logo" alt="Spotify logo" />
-        </a>
-        {/* <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>MusiConn</h1>
-      <div className="card">
-        <button className='spotifyLogin' onClick={openAuthPage}>
-          Login with Spotify
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+    
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import spotifyLogo from "../assets/spotify.svg"
+import "../App.css"
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="musiconn">MusiConn</div>
+      <button className="logInWithSpotifyWrapper ">
+        <div className="logInWith">Log in with Spotify</div>
+      </button>       
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={spotifyLogo} className="logo" alt="Spotify logo" />
+        </a>
+      </div>
+      <h1>MusiConn</h1>
+      <div className="desc-container">
+        <div className="desc">
+            Connect with people who love the same song, share how it makes you feel,
+            or just enjoy a moment together
+        </div>
+      </div>
+      <div className="card">
+        <button className='spotifyLogin'>
+          Login with Spotify
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
